352ea57a4d3b63d992d45bf2ef03402fb28ba98c
Thomas Castagna <castagna.tm@gmail.com>
2019-08-07 20:44:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,3 +5,44 @@ def map_to_negativize(number_array)
   end
   return number_array
 end
+
+def map_to_no_change(array)
+  return array
+end
+
+def map_to_double(number_array)
+  number_array.length.times do |index|
+    number_array[index] *= 2
+  end
+  return number_array
+end
+
+def map_to_square(number_array)
+  number_array.length.times do |index|
+    number_array[index] *= number_array[index]
+  end
+  return number_array
+end
+
+def reduce_to_total(number_array, starting_point = 0)
+  sum = 0
+	index = starting_point
+  while number_array[index] do
+    sum += number_array[index]
+		index += 1
+  end
+  return sum
+end
+
+def reduce_to_all_true(number_array)
+  result = true
+  number_array.length.times do |index|
+    if number_array[index] == false
+      result = false
+    end
+    return result
+end
+
+def reduce_to_any_true(number_array)
+
+end
